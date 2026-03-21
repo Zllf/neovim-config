5923da01eaa84a73dfa8c68ec58abf44df3c89e6
Zllf <Zllf@users.noreply.github.com>
2026-03-22 01:49:33 +0800

    feat(autocmds): 为 markdown 文件禁用诊断、拼写检查和隐藏级别

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,12 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  callback = function()
+    vim.diagnostic.enable(false, { bufnr = 0 })
+    vim.wo.spell = false
+    vim.wo.conceallevel = 0
+  end,
+})
