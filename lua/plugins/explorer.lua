067c69e86178d3818823f9f5ecd7d3fae8062e45
Zllf <Zllf@users.noreply.github.com>
2026-03-22 00:54:23 +0800

    feat(plugins): 添加 leetcode.nvim 并移除 neo-tree.nvim
    - 新增 leetcode.nvim 插件配置，支持力扣中国站和 Go 语言
    - 支持 nvim leetcode.nvim 和 :Leet 两种启动方式
    - 移除已禁用的 neo-tree.nvim 配置

--- lua/plugins/explorer.lua
+++ lua/plugins/explorer.lua
@@ -1,6 +1,4 @@
 return {
-  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
-
   {
     "folke/snacks.nvim",
     opts = {
---
+++ lua/plugins/leetcode.lua
@@ -0,0 +1,26 @@
+local leet_arg = "leetcode.nvim"
+
+return {
+  {
+    "kawre/leetcode.nvim",
+    lazy = leet_arg ~= vim.fn.argv(0, -1),
+    cmd = "Leet",
+    build = ":TSInstall html",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+    },
+    opts = {
+      arg = leet_arg,
+      lang = "go",
+      cn = {
+        enabled = true,
+        translator = true,
+        translate_problems = true,
+      },
+      plugins = {
+        non_standalone = true,
+      },
+    },
+  },
+}
